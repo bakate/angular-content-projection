ea592de589cc0108e0cbb568a7f4f53f9ba96fa0
Bakate <bakateba@gmail.com>
2026-06-13 13:15:39 +0200

    feat(app): add complete content projection demo with angular 22 modern syntax

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, TemplateRef, viewChild } from '@angular/core';
+import { SimpleCard } from './shared/ui/simple-card/simple-card';
+import { ButtonModule } from 'primeng/button';
+import { Heading } from './shared/ui/heading/heading';
+import { Card } from './shared/ui/card/card';
+import { NgOptimizedImage, NgTemplateOutlet } from '@angular/common';
+import { List } from './shared/ui/list/list';
+import { Modal } from './shared/ui/modal/modal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SimpleCard, ButtonModule, Heading, Card, NgOptimizedImage, List, Modal, NgTemplateOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-templating');
+  showAlert() {
+    alert('clicked');
+  }
+
+  modalVisible = signal(false);
+  currentView = signal<'home' | 'about' | 'contact'>('home');
+  items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ];
 }
+
