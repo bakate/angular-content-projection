b213cd433cead8b29b826b741771df11ede07bf4
Bakate <bakateba@gmail.com>
2026-06-13 13:15:12 +0200

    feat(heading): add heading component with content projection

---
+++ src/app/shared/ui/heading/heading.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'heading',
+  imports: [],
+  templateUrl: './heading.html',
+  styleUrl: './heading.css',
+})
+export class Heading {
+  title = input.required<string>();
+  level = input< "lg"| "xl">("xl");
+}
